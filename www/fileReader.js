1a0e97e64a1d420abc69e013a8fd3c0d665718ac
Joey <joseph_buono@uri.edu>
2021-09-18 21:46:10 -0400

    cleaned up rust files

---
+++ www/fileReader.js
@@ -0,0 +1,15 @@
+function init(){
+    document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
+   
+  }
+  
+  function handleFileSelect(event){
+    const reader = new FileReader()
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0])
+  }
+  
+  function handleFileLoad(event){
+    document.getElementById('fileContent').textContent = event.target.result;
+    return document.getElementById('fileContent').textContent;
+  }
